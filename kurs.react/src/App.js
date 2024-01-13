c181a04496e592e693cb2140e57259a5657d4626
OskarPodrucki <oskar.podrucki1@gmail.com>
2024-01-13 09:17:22 +0100

    ...

--- kurs.react/src/App.js
+++ kurs.react/src/App.js
@@ -1,7 +1,8 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import Create from "./components/Create"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Create from "./components/Create";
+import BlogDetails from "./components/BlogDetails";
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 					<Routes>
 						<Route exact path='/' element={<Home />}></Route>
 						<Route exact path='/create' element={<Create />}></Route>
+						<Route exact path='/blogs/:id' element={<BlogDetails />}></Route>
 					</Routes>
 				</div>
 			</div>
